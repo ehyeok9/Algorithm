e2fe6dfee9f8a42523a13017b95bc34d7ad159c9
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-11 14:50:49 +0000

    TIS

--- CodeUp/1476.cpp
+++ CodeUp/1476.cpp
@@ -10,27 +10,14 @@ int main(){
 	cin >> n >> m;
 	int list[n+1][m+1];
 	int x = 1, y = 1;
-	int a =1;
+	int a =1, x_cnt = 1, y_cnt = 1;
 	
-	for (int i=1; i <= n*2; i++){
-		if (i > n){
-			x = n;
-			y = i - n +1;
-			for (int j = n*2; j >= i; j--){
-				list[x][y] = a;
+	for (int i=1; i <= n+m-1; i++){
+		for (int j=1; j <= n; j++){
+			int q = i-j;
+			if (q >= 0 && q < m){
+				list[j][q] = a;
 				a++;
-				x--;
-				y++;
-			}
-		}
-		else{
-			x = i;
-			y = 1;
-			for (int j = 1; j <= i; j++){
-				list[x][y] = a;
-				a++;
-				x--;
-				y++;
 			}
 		}
 	}
