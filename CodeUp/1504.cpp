e96af54f2b3a98538c10a9718d5ca7473ca28c49
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-14 14:51:27 +0000

    지그재그 배열 2

---
+++ CodeUp/1504.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int n, a= 1;
+	scanf("%d", &n);
+	int list[n+1][n+1];
+	
+	for (int i=1; i <= n; i++){
+		if (i%2 == 0){
+			for (int j=n; j >= 1; j--){
+				list[j][i] = a;
+				a++;
+			}
+		}
+		else{
+			for (int j=1; j <= n; j++){
+				list[j][i] = a;
+				a++;
+			}
+		}
+	}
+	
+	for (int i=1; i <= n; i++){
+		for (int j=1; j <= n; j++){
+			printf("%d ", list[i][j]);
+		}
+		cout << endl;
+	}
+	return 0;
+}
