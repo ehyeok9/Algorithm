fa0dd8873e2b53f5ecf29c29c39c0ec2fc0f7610
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-15 14:48:31 +0000

    [기초-배열연습] 2차원 배열 빗금 채우기 3-6

---
+++ CodeUp/1481.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int a = 1;
+	int list[n+1][m+1];
+	
+	for (int i=n+m; i >=2; i--){
+		for (int j=1; j<=m; j++){
+			for (int k=1; k<=n; k++){
+				if (j+k == i){
+					list[k][j]= a++;
+				}
+			}
+		}
+	}
+	
+	for (int i=1; i <=n; i++){
+		for (int j=1; j <=m; j++){
+			printf("%d ", list[i][j]);
+		}
+		cout << endl;
+	}
+	return 0;
+}
