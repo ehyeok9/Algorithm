35328cd7b4be53c800ec7cb8382e5a9371d6c689
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-20 14:51:45 +0000

    숫자 등고선

---
+++ CodeUp/1512.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int n, x, y;
+	scanf("%d", &n);
+	scanf("%d %d", &x, &y);
+	int list[101][101];
+	list[x][y] = 1;
+	
+	for (int i=2; i <= 100; i++){
+		
+		int x_1 = -i+1, y_1 =0, x_2 = -i+1, y_2 =0, x_3 = i-1, y_3=0, x_4= i-1, y_4 =0;
+		
+		for(int j=i; j>=1; j--){
+			if ((x+x_1 < 1) || (x+x_3 > 100) || (y+y_1 < 1) || (y+y_4 > 100)){
+				x_1++;
+				y_1--;	
+				x_2++;
+				y_2++;
+				x_3--;
+				y_3--;
+				x_4--;
+				y_4++;
+				continue;
+			}			
+			list[x+x_1][y+y_1] = i;
+			x_1++;
+			y_1--;
+			list[x+x_2][y+y_2] = i;
+			x_2++;
+			y_2++;
+			list[x+x_3][y+y_3] = i;
+			x_3--;
+			y_3--;
+			list[x+x_4][y+y_4] = i;
+			x_4--;
+			y_4++;
+		}
+	}
+	
+	for (int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			printf("%d ", list[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
