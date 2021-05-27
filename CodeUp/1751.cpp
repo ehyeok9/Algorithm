c945b6d7b090d1b42201958fe7c33e232f210315
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-27 14:47:46 +0000

    글자수 세기

---
+++ CodeUp/1751.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+	string str;
+	getline(cin, str);
+	printf("%ld", str.length());
+	
+	return 0;
+}
