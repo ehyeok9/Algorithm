2e8e75605f8cb1fc94090f12b3f17ec80faabba0
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-28 14:43:37 +0000

    [기초-배열연습] 두 개씩 묶어 큰 값 골라 배열 만들기 5-6

---
+++ CodeUp/1401-1500/1497.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int n, g, a;
+	scanf("%d %d", &n, &g);
+	int list[n+1];
+	
+	for (int i=1; i <=n; i++){
+		scanf("%d", &a);
+		list[i] = a;
+	}
+	
+	vector<int> result;
+	int minimum;
+	
+	for (int i=1; i <=n; i++){
+		if (i ==1){
+			minimum = list[i];
+		}
+		if (g != 1){
+			if (i == n){
+				if (minimum < list[i]){
+					result.push_back(minimum);
+				}
+				else{
+					result.push_back(list[i]);
+				}
+			}
+			else if (i % g == 0){
+				if (minimum < list[i]){
+					result.push_back(minimum);
+				}
+				else{
+					result.push_back(list[i]);
+				}
+				if (i < n){
+					minimum = list[i+1];
+				}
+			}
+			else{
+				if (minimum > list[i]){
+					minimum = list[i];
+				}
+			}
+		}
+		else {
+			result.push_back(list[i]);
+		}
+	}
+	
+	for (int i=0; i < result.size(); i++){
+		printf("%d ", result[i]);
+	}
+	
+	return 0;
+}
