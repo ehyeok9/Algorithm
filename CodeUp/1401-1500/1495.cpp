86c4bb432e5012267ef9ebd13cfc4793699e7e3f
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-05-31 14:49:18 +0000

    10점짜리 ..

--- CodeUp/1401-1500/1495.cpp
+++ CodeUp/1401-1500/1495.cpp
@@ -9,41 +9,46 @@ int main(){
 	int row, col, n;
 	scanf("%d %d %d", &row, &col, &n);
 	int x1, y1, x2, y2, u;
-	int list[row+1][col+1] = {0}, summary[row+1][col+1] = {0};
+	int list[row][col] = {}, summary[row][col] = {};
 	
-	for (int i=0 ;i<n; i++){
+	for (int i=1 ;i<=n; i++){
 		scanf("%d %d %d %d %d", &x1, &y1, &x2, &y2, &u);
-		list[x1][y1] += u;
-		list[x2+1][y2+1] += u;
-		list[x1][y2+1] -= u;
-		list[x2+1][y1] -= u;
+		list[x1][y1] = list[x1][y1] + u;
+		list[x2+1][y2+1] = list[x2+1][y2+1] + u;
+		
+		list[x1][y2+1] = list[x1][y2+1] - u;
+		list[x2+1][y1] = list[x2+1][y1] - u;
 	}
 	
-	for (int i=1; i<=row; i++){
-		for (int j=1; i<=col; j++){
-			if (j==1){
+	for (int i=0; i<row; i++){
+		for (int j=0; j<col; j++){
+			if (j==0){
 				summary[i][j] = list[i][j];
+				for (int k=0; k< i; k++){
+					summary[i][j] += list[k][j];
+				}
 			}
 			else{
 				summary[i][j] = list[i][j] + summary[i][j-1];
+				for (int k=0; k< i; k++){
+					summary[i][j] += list[k][j];
+				}
 			}
 		}
 	}
 	
-	for (int i=1; i <= row; i++){
-		for (int j=1; j <= col; j++){
-			printf("%d", list[i][j]);
-			cout << 1;
+	for (int i=0; i < row; i++){
+		for (int j=0; j < col; j++){
+			printf("%d ", list[i][j]);
 		}
 		cout << endl;
 	}
 	
 	cout << endl;
-	cout << 1;
-	for (int i=1; i <= row; i++){
-		for (int j=1; j <= col; j++){
-			printf("%d", summary[i][j]);
-			cout << 2;
+	
+	for (int i=0; i < row; i++){
+		for (int j=0; j < col; j++){
+			printf("%d ", summary[i][j]);
 		}
 		cout << endl;
 	}
