3b4c988a95c8171c15a9a3975328564a9ab9b9a7
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-06-04 14:37:30 +0000

    5점짜리 ..

--- CodeUp/1515.cpp
+++ CodeUp/1515.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main(){
-	int list[26][26];
+	int list[26][26], list_2[26][26] = {};
 	int a;
 	
 	for (int i=1; i<=25; i++){
@@ -22,45 +22,117 @@ int main(){
 			if (i == 1 && j == 1){
 				cnt = list[i+1][j] + list[i][j+1] + list[i+1][j+1];
 				if (list[i][j] == 0 && cnt == 3){
-					list[i+1][j] = 1;
+					list_2[i][j] = 1;
 				}
-				else if (list[i][j] == 1 && (cnt >= 4 || cnt == 1)){
-					list[i+1][j] = 0;
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
 				}
 				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
-					list[i+1][j] = 1;
+					list_2[i][j] = 1;
 				}
 			}
 			else if (i ==1 && j == 25){
 				cnt = list[i+1][j] + list[i][j-1] + list[i+1][j-1];
 				if (list[i][j] == 0 && cnt == 3){
-					list[i+1][j] = 1;
+					list_2[i][j] = 1;
 				}
-				else if (list[i][j] == 1 && (cnt >= 4 || cnt == 1)){
-					list[i+1][j] = 0;
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
 				}
 				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
-					list[i+1][j] = 1;
+					list_2[i][j] = 1;
+				}
+			}
+			else if (i == 25 && j == 1){
+				cnt = list[i-1][j] + list[i-1][j+1] + list[i][j+1];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
+				}
+			}
+			else if (i == 25 && j == 25){
+				cnt = list[i-1][j] + list[i-1][j-1] + list[i][j-1];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
+				}
+			}
+			else if (i == 25){
+				cnt = list[i-1][j-1] + list[i-1][j] + list[i-1][j+1] + list[i][j-1] + list[i][j+1];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
 				}
 			}
 			else if (i == 1){
-				
+				cnt = list[i][j-1] + list[i][j+1] + list[i+1][j-1] + list[i+1][j] + list[i+1][j+1];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
+				}
 			}
 			else if (j == 1){
-				
+				cnt = list[i-1][j] + list[i-1][j+1] + list[i][j+1] + list[i+1][j+1] + list[i+1][j];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
+				}
 			}
 			else if (j == 25){
-				
+				cnt = list[i-1][j-1] + list[i][j-1] + list[i+1][j-1] + list[i-1][j] + list[i+1][j];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
+				}
 			}
 			else{
-				
+				cnt = list[i-1][j-1] + list[i-1][j] + list[i-1][j+1] + list[i][j-1] + list[i][j+1] + list[i+1][j-1] + list[i+1][j] + list[i+1][j+1];
+				if (list[i][j] == 0 && cnt == 3){
+					list_2[i][j] = 1;
+				}
+				else if (list[i][j] == 1 && (cnt >= 4 || cnt <= 1)){
+					list_2[i][j] = 0;
+				}
+				else if (list[i][j] == 1 && (cnt == 2 || cnt == 3)){
+					list_2[i][j] = 1;
+				}
 			}
 		}
 	}
 	
 	for (int i=1; i<=25; i++){
 		for(int j=1; j<=25; j++){
-			printf("%d ", list[i][j]);
+			printf("%d ", list_2[i][j]);
 		}
 		printf("\n");
 	}
