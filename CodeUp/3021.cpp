59c844faad3e59d9b92328aaeaa533f849a03cee
­이혁규[소프트웨어학부] <e_hyeok9@kookmin.ac.kr>
2021-07-07 14:41:03 +0000

    큰 수 덧셈

--- CodeUp/3021.cpp
+++ CodeUp/3021.cpp
@@ -2,152 +2,85 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <cmath>
 #include <stack>
 using namespace std;
 
 int main(){
 	string a,b;
 	cin >> a >> b;
-	
-	stack<char> first, second;
-	for (int i=0; i <a.length(); i++){
-		first.push(a[i]);
-	}
-	for (int i=0; i <b.length(); i++){
-		first.push(b[i]);
-	}
-	
-	int x,y,c;
-	string result = "";
 	bool flag = false;
-	
-	if (first.size() == second.size()){
-		while(!first.empty()){
-			x = first.top() - '0';
-			y = second.top() - '0';
-			
-			if(flag){
-				c = x+y+1;
-			}
-			else{
-				c = x+y;
-			}
-			
-			if (first.size() == 1){
-				if (c > 9){
-					result += to_string(c%10);
-					result += "1";
-				}
-				else{
-					result += to_string(c);
-				}
-			}
-			else if (c > 9){
-				result += to_string(c%10);
-				flag = true;
-			}
-			else{
-				result += to_string(c);
-				flag = false;
-			}			
-			first.pop();
-			second.pop();
+	string result = "";
+	int fir=0, sec=0;
+	int leng;
+	if (a.length() > b.length()){
+		leng = a.length() - b.length();
+		for (int i=0; i< leng; i++){
+			b.insert(0, "0");
+		}
+	}
+	else if (a.length() < b.length()){
+		leng = b.length() - a.length();
+		for (int i=0; i< leng; i++){
+			a.insert(0, "0");
 		}
 	}
 	else{
-		if (first.size() > second.size()){
-			while(!first.empty()){
-				if (second.empty()){
-					if (flag){
-						x = first.top() - '0' + 1;
-						if (x > 9){
-							result += to_string(x%10);
-							flag = true;
-						}
-						else{
-							result += to_string(x);
-							flag = false;
-						}
-					}
-					else{
-						x = first.top() - '0';
-						result += to_string(x);
-					}
-					first.pop();
+		leng = 0;
+	}
+	
+	for (int i = max(a.length(), b.length())-1; i > -1; i--){
+		if (flag){
+			fir = a[i] - '0';
+			sec = b[i] - '0';
+			if ((fir + sec + 1 >= 10) && i != 0){
+				result += to_string((fir+sec+1)%10);
+				flag = true;
+			}
+			else if (i == 0){
+				if (fir+sec+1 >=10){
+					result += to_string((fir+sec+1)%10);
+					int re = (fir+sec+1)/10;
+					result += to_string(re%10);
 				}
 				else{
-					x = first.top() - '0';
-					y = second.top() - '0';
-
-					if(flag){
-						c = x+y+1;
-					}
-					else{
-						c = x+y;
-					}
-
-					if (c > 9){
-						result += to_string(c%10);
-						flag = true;
-					}
-					else{
-						result += to_string(c);
-						flag = false;
-					}			
-					first.pop();
-					second.pop();
+					result += to_string(fir+sec+1);
+					flag = false;
 				}
+			}
+			else{
+				result += to_string(fir+sec+1);
+				flag = false;
 			}
 		}
 		else{
-			while(!second.empty()){
-				if (first.empty()){
-					if (flag){
-						y = second.top() - '0' + 1;
-						if (x > 9){
-							result += to_string(x%10);
-							flag = true;
-						}
-						else{
-							result += to_string(x);
-							flag = false;
-						}
-					}
-					else{
-						y = second.top() - '0';
-						result += to_string(x);
-					}
-					second.pop();
+			fir = a[i] - '0';
+			sec = b[i] - '0';
+			if ((fir + sec >= 10) && i != 0){
+				result += to_string((fir+sec)%10);
+				flag = true;
+			}			
+			else if (i == 0){
+				if (fir+sec >=10){
+					result += to_string((fir+sec)%10);
+					int re = (fir+sec)/10;
+					result += to_string(re%10);
 				}
 				else{
-					x = first.top() - '0';
-					y = second.top() - '0';
-
-					if(flag){
-						c = x+y+1;
-					}
-					else{
-						c = x+y;
-					}
-
-					if (c > 9){
-						result += to_string(c%10);
-						flag = true;
-					}
-					else{
-						result += to_string(c);
-						flag = false;
-					}			
-					first.pop();
-					second.pop();
+					result += to_string(fir+sec);
+					flag = false;
 				}
+			}
+			else{
+				result += to_string(fir+sec);
+				flag = false;
 			}
 		}
 	}
 	
 	for (int i=result.length()-1; i >-1; i--){
-		printf("%c", result[i]);
+		cout << result[i];
 	}
+	
+	
 	return 0;
 }
