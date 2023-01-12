13cc31727570c5ee9b067723d56b87d3db0f6d67
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-01-13 04:29:36 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.print.DocFlavor.STRING;
+
+class Pair {
+    int first; 
+    int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        while(true){
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            if (a == 0 && b == 0){
+                break;
+            }
+            if (b%a == 0){
+                bw.write("factor");
+            } else if (a%b == 0){
+                bw.write("multiple");
+            } else {
+                bw.write("neither");
+            }
+            bw.write("\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    
+
+    public static void printArray(char[][] arr){
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+        
+    }
+}
