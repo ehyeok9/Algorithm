8d43847c816cf98cd6ac68dec9b3c3d19fe0ae8b
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-01-12 19:45:52 +0900

    [Bronze III] Title: 택시 기하학, Time: 148 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.print.DocFlavor.STRING;
+
+class Pair {
+    int first; 
+    int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int r = Integer.parseInt(br.readLine());
+        bw.write(Math.PI*Math.pow(r, 2) + "\n");
+        bw.write(Math.pow(0-r, 2)+ Math.pow(0-r, 2) + "\n");
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    
+
+    public static void printArray(char[][] arr){
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+        
+    }
+}
