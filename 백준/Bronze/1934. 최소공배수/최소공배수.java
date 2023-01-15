53af9bae7a1dacfe944940051be0ff98906d9fe1
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-01-15 16:59:40 +0900

    [Bronze I] Title: 최소공배수, Time: 172 ms, Memory: 15780 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import javax.print.DocFlavor.STRING;
+
+class Pair {
+    int first; 
+    int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n ; i++){
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            
+            int maxVal = -1;
+            int tmpA, tmpB;
+            if (a> b){
+                tmpA = a; tmpB =b;
+            } else {
+                tmpA = b; tmpB =a;
+            }
+            while(true){
+                int val = tmpA%tmpB;
+                if (val == 0){
+                    maxVal = tmpB;
+                    break;
+                }
+                tmpA = tmpB;
+                tmpB = val;
+            }
+
+            bw.write(a*b/maxVal + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    
+
+    public static void printArray(char[][] arr){
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+        
+    }
+}
