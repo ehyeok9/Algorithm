da897eb8ddfef0318da104786817b0f4642727cd
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-01-17 21:04:14 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+public class Main{
+    static int[] array;
+    static int[] tmp;
+    static boolean[] visited;
+    static int m;
+    static int n;
+    static int t;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        sb = new StringBuilder();
+        tmp = new int[m+1];
+        visited = new boolean[n+1];
+        t = 0;
+        dfs(0);
+        System.out.print(sb);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void dfs(int num){
+        if (num == m) {
+            for (int i=0; i< tmp.length-1; i++){
+                sb.append(tmp[i] + " ");
+            }
+            sb.append("\n");
+            t++;
+            return;
+        }
+        for (int i=1; i<= n; i++){
+            if (visited[i] == false){
+                tmp[num] = i;
+                visited[i] = true;
+                dfs(num+1);
+                for (int j=i+1; j<=n; j++){
+                    visited[j] = false;
+                }
+            }
+        }
+        return;
+    }
+}
