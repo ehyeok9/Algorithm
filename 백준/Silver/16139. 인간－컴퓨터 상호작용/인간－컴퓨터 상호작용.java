3a7f5e08c4b0ead92f57e2bf0ae7ea87fcbc43d7
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-01-26 17:49:43 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 200 ms, Memory: 17160 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -14,27 +14,34 @@ public class Main{
         
         String query = br.readLine();
         int tcase = Integer.parseInt(br.readLine());
-        
+        int[][] sumVal = new int[query.length()+1][];
+        int[] tmp = new int[26];
+        sumVal[0] = tmp.clone();
+        for (int i=1; i <= query.length(); i++){
+            int num = query.charAt(i-1) - 'a';
+            tmp[num] += 1;
+            sumVal[i] = tmp.clone();
+        }
+
         for (int i=0; i < tcase; i++){
             String[] input = br.readLine().split(" ");
-            String str = query.substring(Integer.parseInt(input[1]), Integer.parseInt(input[2])+1);
-            bw.write(countString(str, input[0])+"\n");
+            int num = input[0].charAt(0) - 'a';
+            int a = Integer.parseInt(input[1]);
+            int b = Integer.parseInt(input[2]);    
+            // System.out.println(String.format("(a=%d, b=%d, valA=%d, valB=%d) ", a, b, sumVal[a][num], sumVal[b+1][num]));
+            bw.write(sumVal[b+1][num] - sumVal[a][num] + "\n");
         }
         
         bw.flush();
         bw.close();
         br.close();    
     }
-
-    public static int countString(String value, String alpha){
-        char tmp = alpha.charAt(0);
-        // System.out.println(value);
-        int result = 0;
-        for (char i : value.toCharArray()){
-            if (tmp == i){
-                result++;
-            }
+    
+    public static void printArray(int[] tmp){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<tmp.length; i++){
+            sb.append(tmp[i] + " ");
         }
-        return result;
+        System.out.println(sb);
     }
 }
