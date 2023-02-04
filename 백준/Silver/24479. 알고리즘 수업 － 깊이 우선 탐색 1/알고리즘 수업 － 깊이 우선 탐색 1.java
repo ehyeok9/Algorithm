2034b1d9d6c1272c7862334b3ff71ea8fd4e813b
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-02-04 22:39:06 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1372 ms, Memory: 143776 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+
+public class Main{
+    static int[] visited;
+    static Deque<Integer> deque;
+    static HashMap<Integer, ArrayList<Integer>> map;
+    static int next = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        int r = Integer.parseInt(input[2]);
+
+        map = new HashMap<>();
+        for (int i=0; i<m; i++){
+            String[] in = br.readLine().split(" ");
+            int key = Integer.parseInt(in[0]);
+            int value = Integer.parseInt(in[1]);
+            
+            ArrayList<Integer> tmp;
+            if (map.containsKey(key)){
+                tmp = map.get(key);
+            } else{
+                tmp = new ArrayList<>();
+            }
+            tmp.add(value);
+            map.put(key, tmp);
+
+            if (map.containsKey(value)){
+                tmp = map.get(value);
+            } else {
+                tmp = new ArrayList<>();
+            }
+            tmp.add(key);
+            map.put(value, tmp);
+        }
+
+        deque = new ArrayDeque<>();
+        visited = new int[n+1];
+        dfs(r);
+        for (int i=1; i<=n; i++){
+            bw.write(visited[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();    
+    }
+
+    public static void dfs(int node){
+        visited[node] = next++;
+        ArrayList<Integer> tmp = map.get(node);
+        if (tmp != null){
+            Collections.sort(tmp);
+            for (int x : tmp){
+                if (visited[x] == 0){
+                    dfs(x);
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] tmp){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<tmp.length; i++){
+            sb.append(tmp[i] + " ");
+        }
+        System.out.println(sb);
+    }
+}
