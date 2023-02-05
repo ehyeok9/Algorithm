925b30df3810b52d559f6d60698189a6db1aec17
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-02-05 21:31:32 +0900

    [Silver II] Title: 최대 힙, Time: 372 ms, Memory: 26592 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+
+public class Main{
+    static int[] array;
+    static boolean[] visited;
+    static int[] dy = {-1, 1};
+    static int xrange = 100001;
+    static int end;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i=0; i<n; i++){
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0){
+                if (queue.isEmpty()){
+                    bw.write(0 + "\n");
+                } else{
+                    bw.write(queue.poll() + "\n");
+                }
+            } else {
+                queue.add(num);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();    
+    }
+
+    public static void printArray(int[][] tmp){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<tmp.length; i++){
+            for (int j=0; j<tmp[i].length; j++){
+                sb.append(tmp[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
