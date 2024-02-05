83227e6b00bc1592090d6258edd0d28be6ae5214
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-02-05 15:19:02 +0900

    [Silver III] Title: N과 M (8), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+global N, M, arr
+global visited
+
+def backTracking(count, num):
+    if len(count) == M:
+        print(*count)
+        return
+    
+    for i in range(num, N):
+        count.append(arr[i])
+        backTracking(count, i)
+        count.pop()
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    
+    visited = [False for _ in range(N)]
+    backTracking([], 0)
