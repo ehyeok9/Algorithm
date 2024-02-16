ba0df16881740be454af2dcd8bdc0dd44c945722
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-02-16 11:47:44 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver I/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver I/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -2,70 +2,45 @@ import sys
 
 input = sys.stdin.readline
 
-global numbers, operators, N, visited
-global maxi, mini
 
-def backtracking(ops):
-    global numbers, operator, N, visited
+
+def dfs(numbers, prev, idx):
+    global add, sub, mul, div, N
     global maxi, mini
     
-    if len(ops) == N-1:
-        temp = getResult(ops, numbers)
-        maxi = max(maxi, temp)
-        mini = min(mini, temp)
-        return 
-
-    for i in range(N-1):
-        if visited[i] == False:
-            ops.append(operators[i])
-            visited[i] = True
-            backtracking(ops)
-            visited[i] = False
-            ops.pop()
-
-def getResult(ops, numbers):
-    result = numbers[0]
+    if idx == N:
+        maxi = max(maxi, prev)
+        mini = min(mini, prev)
+        return
     
-    for i in range(1,N):
-        result = calc(ops[i-1], result, numbers[i])
-        
-    return result    
-
-def calc(op, n1, n2):
-    if op == 0:
-        return n1 + n2
-    elif op == 1:
-        return n1 - n2
-    elif op == 2:
-        return n1 * n2
-    elif op == 3:
-        if n1 < 0:
-            return -(-n1 // n2)
-        return n1 // n2
-    
-    
-def getOperators(operator):
-    result = []
-    
-    for i in range(4):
-        while operator[i] > 0:
-            result.append(i)
-            operator[i] -= 1
-            
-    return result
-
+    if add > 0:
+        add -= 1
+        dfs(numbers, prev + numbers[idx], idx + 1)
+        add += 1
+    if sub > 0:
+        sub -= 1
+        dfs(numbers, prev - numbers[idx], idx + 1)
+        sub += 1
+    if mul > 0:
+        mul -= 1
+        dfs(numbers, prev * numbers[idx], idx + 1)
+        mul += 1
+    if div > 0:
+        div -= 1
+        temp = 0
+        if prev < 0:
+            temp = -(-prev // numbers[idx])
+        else:
+            temp = prev // numbers[idx]
+        dfs(numbers, temp, idx + 1)
+        div += 1
         
 if __name__ == "__main__":
     N = int(input())
     numbers = list(map(int, input().split()))
-    operator = list(map(int, input().split()))
+    add, sub, mul, div = map(int, input().split())
     
-    operators = getOperators(operator)
-    visited = [False for _ in range(N-1)]
-    
-    maxi, mini = -float("inf"), float("inf") 
-    backtracking([])
-    
+    maxi, mini = -float("inf"), float("inf")
+    dfs(numbers, numbers[0], 1)
     print(maxi)
     print(mini)
-    
