49868852a290e2d7e8f45427a933912963dd84d0
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2026-01-25 23:24:43 +0900

    Title: 그래프 탐색, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260125/그래프 탐색/graph-traversal.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+
+def dfs(graph, visited, node, answer):
+    nxt = graph[node]
+    
+    if (nxt is None or nxt == []):
+        return answer
+    
+    for nxtNode in nxt:
+        if not visited[nxtNode]:
+            visited[nxtNode] = True
+            answer = dfs(graph, visited, nxtNode, answer + 1)
+
+    return answer
+
+if __name__=="__main__":
+    N, M = map(int, input().split())
+    graph = {}
+    visited = [False for _ in range(N + 1)]
+    for _ in range(M):
+        x, y = map(int, input().split())
+        graph.setdefault(x, []).append(y)
+        graph.setdefault(y, []).append(x)
+    
+    visited[1] = True
+    print(dfs(graph, visited, 1, 0))
+    
