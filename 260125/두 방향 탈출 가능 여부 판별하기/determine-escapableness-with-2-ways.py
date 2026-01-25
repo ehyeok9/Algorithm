297df7b7e966c110c07ae199ac939750cc25ffe7
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2026-01-25 23:50:01 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260125/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,44 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+matrix = [
+    list(map(int, input().split()))
+    for _ in range(N)
+]
+dy = [0, 1]
+dx = [1, 0]
+visited = [
+    [False for _ in range(N)]
+    for _ in range(N)
+]
+answer = 0;
+
+def inRange(y, x):
+    return (0 <= y < N) and (0 <= x < N)
+
+def canGo(y, x):
+    return matrix[y][x] != 0
+
+def dfs(y, x):
+    global answer
+
+    if (y == (N - 1)) and (x == (N - 1)):
+        answer = 1
+        return
+
+    visited[y][x] = True
+
+    for i in range(2):
+        ny, nx = y + dy[i], x + dx[i]
+        if not inRange(ny, nx): continue
+        if not canGo(ny, nx): continue
+        if visited[ny][nx]: continue
+
+        dfs(ny, nx)
+
+if __name__=="__main__":
+    dfs(0, 0)
+
+    print(answer)
